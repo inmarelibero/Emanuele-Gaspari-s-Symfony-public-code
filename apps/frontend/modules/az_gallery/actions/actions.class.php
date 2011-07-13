1ba3d694821f9aa184f3605eb86ec29a89a578f5
Emanuele Gaspari <inmarelibero@gmail.com>
2011-07-13 21:33:51 +0200

    added az_gallery module in frontend app

---
+++ apps/frontend/modules/az_gallery/actions/actions.class.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * gallery actions.
+ *
+ * @package    
+ * @subpackage gallery
+ * @author     Emanuele Gaspari
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class galleryActions extends sfActions
+{
+	/**
+	 * displays all photos of a gallery
+	 * @param sfWebRequest $request
+	 */
+  public function executePhotos(sfWebRequest $request)
+  {
+		$this->forward404Unless($this->gallery = $this->getRoute()->getObject());
+  }
+  
+  /**
+   * adds a tag to a gallery (ajax call)
+   * 
+   * @param sfWebRequest $request
+   */
+  public function executeAddTag(sfWebRequest $request)
+  {
+  	$this->forward404Unless($gallery = Doctrine_Core::getTable('Gallery')->find($request->getParameter('gallery_id')));
+  	
+  	$tag_name = $request->getParameter('tag_name');
+  	if ($tag_name != '')
+  	{
+			$gallery->addTag($tag_name);
+			$gallery->save();
+  	}
+  	exit;
+  }
+  
+  /**
+   * removes a tag of a gallery (ajax call)
+   * 
+   * @param sfWebRequest $request
+   */
+  public function executeRemoveTag(sfWebRequest $request)
+  {
+  	$this->forward404Unless($gallery = Doctrine_Core::getTable('Gallery')->find($request->getParameter('gallery_id')));
+  	
+  	$tag_name = $request->getParameter('tag_name');
+  	if ($tag_name != '')
+  	{
+			$gallery->removeTag($tag_name);
+			$gallery->save();
+  	}
+  	exit;
+  }
+}
---
+++ apps/frontend/modules/az_gallery/templates/photosSuccess.php
@@ -0,0 +1,81 @@
+<script>
+	// equal size to some elements
+	$(document).ready(function(){
+		photosToEqualHeights();
+	});
+</script>
+
+<script type="text/javascript">
+	$(function() {
+		// initializes the slideshow (lightbox)
+		$('.photos_list a').lightBox({
+			overlayBgColor: '#000',
+			overlayOpacity: 0.6,
+			imageLoading: '/images/lightbox-ico-loading.gif',
+			imageBtnClose: '/images/lightbox-btn-close.gif',
+			imageBtnPrev: '/images/lightbox-btn-prev.gif',
+			imageBtnNext: '/images/lightbox-btn-next.gif',
+			containerResizeSpeed: 350,
+		});
+	});
+</script>
+
+
+<div class="gallery_title">
+	<div>
+		<?php echo __('Title')?>:
+	</div>
+	<div>
+		<?php echo $gallery->getTitle()?>
+	</div>
+</div>
+
+<?php if ($gallery->getDescrizione() != ''):?>
+	<div class="gallery_description">
+		<div>
+			<?php echo __('Description')?>:
+		</div>
+		<div>
+			<?php echo html_entity_decode($gallery->getDescription())?>
+		</div>
+	</div>
+<?php endif;?>
+
+<?php if ($gallery->getCategory()):?>
+	<div class="gallery_category">
+		<div>
+			<?php echo __('Category')?>:
+		</div>
+		<div>
+			<?php echo $gallery->getCategory()->getName()?>
+		</div>
+	</div>
+<?php endif;?>
+
+<?php $tags = $gallery->getTags(); ?>
+<?php if (count($tags) > 0):?>
+	<div class="gallery_tags">
+		<div>
+			<?php echo __('Tags')?>:
+		</div>
+		<div>
+			<?php $i = 0;?>
+			<?php foreach ($tags as $tag):?>
+				<?php echo ($i < count($tags)-1) ? $tag.',' : $tag ?>
+				<?php $i++?>
+			<?php endforeach;?>
+		</div>
+	</div>
+<?php endif;?>
+
+<div class="photos_list">
+	<?php foreach($gallery->getPhotoes() as $key => $photo):?>
+		<div class="photo_box">
+			<div class="image">
+				<a href="/uploads/<?php echo sfConfig::get('app_photos_folder').'/'.$photo->getFilename()?>">
+					<?php echo image_tag($photo->getThumbnailImageTag())?>
+				</a>
+			</div>
+		</div>
+	<?php endforeach;?>
+</div>
