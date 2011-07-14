ef34a7d7dd3ac4d2227a52f3dd4ac7857becc9d6
Emanuele Gaspari <inmarelibero@gmail.com>
2011-07-14 10:29:01 +0200

    added controllers

---
+++ frontend_dev.php
@@ -0,0 +1,35 @@
+<?php
+
+// this check prevents access to debug front controllers that are deployed by accident to production servers.
+// feel free to remove this, extend it or make something more sophisticated.
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
+{
+  die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
+
+/**
+ * change require_once in order to reflect new folder organization of symfony project
+ *
+ * the new structure would be:
+ * 	| - cache
+ * 	| - code
+ * 			| - apps
+ * 			| - config
+ * 			| - data
+ * 			| - lib
+ * 			| - plugins
+ * 			\ - test
+ * 	| - css
+ * 	| - images
+ * 	| - js
+ * 	| - log
+ * 	| - uploads
+ * 	| - index.php
+ * 	| - frontend_dev.php
+ * 	| - backend.php
+ * 	\ - backend_dev.php
+ */
+require_once(dirname(__FILE__).'/code/config/ProjectConfiguration.class.php');
+
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
+sfContext::createInstance($configuration)->dispatch();
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * change require_once in order to reflect new folder organization of symfony project
+ *
+ * the new structure would be:
+ * 	| - cache
+ * 	| - code
+ * 			| - apps
+ * 			| - config
+ * 			| - data
+ * 			| - lib
+ * 			| - plugins
+ * 			\ - test
+ * 	| - css
+ * 	| - images
+ * 	| - js
+ * 	| - log
+ * 	| - uploads
+ * 	| - index.php
+ * 	| - frontend_dev.php
+ * 	| - backend.php
+ * 	\ - backend_dev.php
+ */
+require_once(dirname(__FILE__).'/code/config/ProjectConfiguration.class.php');
+
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
+sfContext::createInstance($configuration)->dispatch();
